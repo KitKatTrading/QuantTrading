4461565e10f1f6add986d095295a709cb10c5cf8
Kitkat <kitkattrading@gmail.com>
2023-12-19 19:32:02 -0800

    temp update for Ray to run demo

--- run_strategy_chanlun_hub_4h.py
+++ run_strategy_chanlun_hub_4h.py
@@ -1,11 +1,12 @@
 from Objects.strategy import Strategy
 
-strategy_chanlun_12h = Strategy(name_symbol='OPUSDT', data_source='binance', name_strategy='chanlun_12h',
-                                timeframe_high='1d', timeframe_mid='4h', timeframe_low='1h',
-                                function_high_timeframe='SMA_5_10_20_trend',
-                                function_mid_timeframe='chanlun_central_hub',
-                                function_low_timeframe='RSI_divergence')
+strategy_chanlun = Strategy(name_symbol='OPUSDT', data_source='binance', name_strategy='chanlun_12h',
+                            timeframe_high='1d', timeframe_mid='4h', timeframe_low='1h',
+                            function_high_timeframe='always_long',
+                            function_mid_timeframe='chanlun',
+                            function_low_timeframe='RSI_extreme_cross')
 
-trading_decision = strategy_chanlun_12h.check_trading_decision_all_modules()
+trading_decision = strategy_chanlun.run_direction_module_live()
+
 print(trading_decision)
 
