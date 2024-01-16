20eb6693ffcb092b44472a41e41c6cedd0d9f96b
KitKat <112379029+KitKatTrading@users.noreply.github.com>
2024-01-16 09:59:39 -0800

    Update config_binance_vpn.py

--- config_binance_vpn.py
+++ config_binance_vpn.py
@@ -7,8 +7,8 @@ gvars = {
     # 'webhook_ensembler_EN': 'https://discord.com/api/webhooks/1167215111891591198/Z7-f9eTTw_6g0SgweAz78u8n64AQSaLn9_kuSb7gVcPMk5wJ9o50KoIHTGYThuLwUhr4',
     # 'webhook_ensembler_CN': 'https://discord.com/api/webhooks/1167215637542752276/EpX8uQVCmSduVOZabAC31vjcMnFbG3B10JHf_2hYpWKusVEy9F7vDMs-w7lN5oKEKvFn',
     'webhook_ensembler': 'https://discord.com/api/webhooks/1167215637542752276/EpX8uQVCmSduVOZabAC31vjcMnFbG3B10JHf_2hYpWKusVEy9F7vDMs-w7lN5oKEKvFn',
-    'API_KEY': 'W2E6HCVZOw02CFyLN1UenrnBSKpO2DJybasoPR51vUcl3ZqLP9HeTbF4yLWTjKKa',
-    'API_SECRET': 'd4SWo9n2bGIZUhGfF1VAZQ1Yu6zJWnREQ8qd2517QX2m819ArNOsJgptQUNPz4xL',
+    'API_KEY': '',
+    'API_SECRET': '',
 }
 
 
